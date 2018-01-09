bd32dbbf7f5242776b43342641a5f1b78ada6a4d
Amberly <armstrong.amberly@gmail.com>
2018-01-09 17:26:30 -0500

    updated config routes and added welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+  root 'welcome#index'
+
+  resources: plots
+  resources: markers
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
