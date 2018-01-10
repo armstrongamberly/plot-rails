0ab5c5902b5a84cab0ee2e2fa4424de4a05ce487
Amberly <armstrong.amberly@gmail.com>
2018-01-09 21:08:42 -0500

    updated models

--- app/models/marker.rb
+++ app/models/marker.rb
@@ -1,2 +1,4 @@
 class Marker < ApplicationRecord
+	belongs_to :plot
+	belongs_to :user through:, :plot
 end
--- app/models/plot.rb
+++ app/models/plot.rb
@@ -1,2 +1,4 @@
 class Plot < ApplicationRecord
+	belongs_to :user
+	has_many :markers
 end
