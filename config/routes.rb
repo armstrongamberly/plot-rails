d32d842e45a35ea40661e6d035cb6c7f7089be08
Amberly <armstrong.amberly@gmail.com>
2018-01-10 20:34:34 -0500

    need to fix facebook login path

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'welcome/index'
+ 
+  root 'welcome#index'
+
   resources :plots
   resources :markers
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
